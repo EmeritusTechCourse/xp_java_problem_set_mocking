a1321b1a894098f8f6d4e6338d636bb5ca6db24c
David Bieder <mail@david-bieder.com>
2016-11-30 23:08:18 +0000

    Provide implementation for solution

--- src/test/java/com/galvanize/curriculum/xp/orm/OrmTest.java
+++ src/test/java/com/galvanize/curriculum/xp/orm/OrmTest.java
@@ -1,24 +1,39 @@
 package com.galvanize.curriculum.xp.orm;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class OrmTest {
 
     @Test
-    public void blackBoxTestSaveAndLoad() {
+    public void utilizesDriverToSaveObject() {
         // Setup
-        Orm orm = new Orm(new SuperComplexDriver());
-        Object expected = "test";
-        int id = expected.hashCode();
+        MockDriver mockDriver = new MockDriver();
+        Orm orm = new Orm(mockDriver);
 
-        // Setup & exercise
-        orm.save(expected);
-        Object actual = orm.get(id);
+        // Exercise
+        orm.save("A Test String");
 
         // Assert
-        Assert.assertEquals("Orm should save and load", expected, actual);
+        assertThat(mockDriver.saveCalls.size(), is(1));
+        assertThat(mockDriver.saveCalls.get(0), is("A Test String"));
+    }
+
+    @Test
+    public void utilizesDriverToGetObject() {
+        // Setup
+        MockDriver mockDriver = new MockDriver();
+        mockDriver.getWillReturn = "Fake Return Value";
+        Orm orm = new Orm(mockDriver);
+
+        // Exercise
+        Object retrievedValue = orm.get(42);
+
+        // Assert
+        assertThat(mockDriver.getCalls.size(), is(1));
+        assertThat(mockDriver.getCalls.get(0), is(42));
+        assertThat(retrievedValue, is("Fake Return Value"));
     }
 }
