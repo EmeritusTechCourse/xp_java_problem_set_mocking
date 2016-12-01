36fdb41ce4dd78f1fb1c8cc2f26c54002cb11bf7
David Bieder <mail@david-bieder.com>
2016-12-01 00:07:10 +0000

    Provide mockito based solution

--- src/test/java/com/galvanize/curriculum/xp/orm/OrmTest.java
+++ src/test/java/com/galvanize/curriculum/xp/orm/OrmTest.java
@@ -1,24 +1,52 @@
 package com.galvanize.curriculum.xp.orm;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
+@RunWith(MockitoJUnitRunner.class)
 public class OrmTest {
 
+    @Mock
+    Driver mockDriver;
+
     @Test
-    public void blackBoxTestSaveAndLoad() {
-        // Setup
-        Orm orm = new Orm(new SuperComplexDriver());
-        Object expected = "test";
-        int id = expected.hashCode();
+    public void utilizesDriverToSaveObject() {
+        Orm orm = new Orm(mockDriver);
 
-        // Setup & exercise
-        orm.save(expected);
-        Object actual = orm.get(id);
+        orm.save("A Test String");
 
-        // Assert
-        Assert.assertEquals("Orm should save and load", expected, actual);
+        then(mockDriver).should(times(1)).save("A Test String");
+
+        orm.save(new int[] { 1, 2, 3});
+
+        then(mockDriver).should(times(2)).save(any());
+        then(mockDriver).should(times(1)).save(new int[] { 1, 2, 3});
+    }
+
+    @Test
+    public void utilizesDriverToGetObject() {
+        given(mockDriver.get(anyInt())).willReturn("Fake Return Value");
+        Orm orm = new Orm(mockDriver);
+
+        Object retrievedValue = orm.get(42);
+
+        then(mockDriver).should(times(1)).get(42);
+        assertThat(retrievedValue, is("Fake Return Value"));
+
+        Object anotherValue = orm.get(99);
+
+        then(mockDriver).should(times(2)).get(anyInt());
+        then(mockDriver).should(times(1)).get(99);
+        assertThat(anotherValue, is("Fake Return Value"));
     }
 }
