d3388834b1082b212e4204bad00522ca0f055f29
jeff-lent <jefflent01@gmail.com>
2022-11-22 23:57:55 -0500

    package changes

---
+++ src/main/java/com/glc/curriculum/xp/orm/Driver.java
@@ -0,0 +1,7 @@
+package com.glc.curriculum.xp.orm;
+
+public interface Driver {
+    void save(Object obj);
+
+    Object get(int id);
+}
---
+++ src/test/java/com/glc/curriculum/xp/orm/OrmTest.java
@@ -0,0 +1,22 @@
+package com.glc.curriculum.xp.orm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class OrmTest {
+
+    @Test
+    public void blackBoxTestSaveAndLoad() {
+        // Setup
+        Orm orm = new Orm(new SuperComplexDriver());
+        Object expected = "test";
+        int id = expected.hashCode();
+
+        // Setup & exercise
+        orm.save(expected);
+        Object actual = orm.get(id);
+
+        // Assert
+        assertEquals(expected, actual, "Orm should save and load");
+    }
+}
